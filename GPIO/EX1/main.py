57e12ee9128bf7ed39e999ce153b2f6a6c85a5c9
sit <alzubaidimohammed0@gmail.com>
2025-10-03 19:16:45 +0200

    Ex1: Blink LED avec interruption sur bouton poussoir

--- GPIO/EX1/main.py
+++ GPIO/EX1/main.py
@@ -4,30 +4,38 @@ from utime import sleep
 # LED sur GPIO16
 led = Pin(16, Pin.OUT)
 
-# Bouton sur GPIO20 (résistance externe déjà câblée)
-btn = Pin(20, Pin.IN)
+# Bouton sur GPIO20 avec résistance interne pull-up
+btn = Pin(20, Pin.IN, Pin.PULL_UP)
 
-mode = 0             # 0 = LED éteinte, 1 = lent, 2 = rapide
-last_state = 0       # état précédent du bouton
-delay_base = 1       # base du délai (1s = 0.5 Hz)
+mode = 0
 
+# Fonction d’interruption
+def bouton_irq(pin):
+    global mode #fait reference a la variable global mode
+
+    print("click")
+    mode = (mode+1)%3
+    print (f"mode : {mode}")
+
+
+# Ici on déclenche sur front descendant (1 -> 0 = appui du bouton)
+btn.irq(trigger=Pin.IRQ_FALLING, handler=bouton_irq)
+
+# Boucle infinie pour garder le programme en vie
 while True:
-    state = btn.value()
 
-    # Détection d'un appui (front descendant : 0->1)
-    if state == 1 and last_state == 0:
-        mode = (mode + 1) % 3   # cycle entre 0, 1 et 2
-        print("Mode:", mode)
-        sleep(0.2)  # anti-rebond simple
+    if mode == 0 : 
+        led.value(0)
 
-    last_state = state
+    elif mode == 1 : 
+        led.value(1)
+        sleep(0.5)
+        led.value(0)
+        sleep(0.5)
 
-    # Gestion des modes
-    if mode == 0:
-        led.off()
-    elif mode == 1:
-        led.toggle()
-        sleep(delay_base)   # lent = 1 seconde
-    elif mode == 2:
-        led.toggle()
-        sleep(delay_base/4) # rapide = 0.25 seconde
+
+    elif mode == 2 : 
+        led.value(1)
+        sleep(0.25)
+        led.value(0)
+        sleep(0.25)
